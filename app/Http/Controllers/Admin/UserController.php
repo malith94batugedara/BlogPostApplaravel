16bbec44ea78a5d8fe9b440495013ee27c295382
malith94batugedara <malithbatugedara@gmail.com>
2024-04-02 12:07:17 +0530

    users roles change done

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -12,4 +12,22 @@ class UserController extends Controller
         $users=User::all();
         return view('admin.users.index',compact('users'));
     }
+
+    public function edit($user_id){
+        $user=User::findOrFail($user_id);
+        return view('admin.users.edit',compact('user'));
+    }
+
+    public function update(Request $request,$user_id){
+
+        $user=User::findOrFail($user_id);
+        if($user){
+            $user->role=$request->role;
+            $user->update();
+            return redirect(route('admin.users'))->with('status','User Updated Successfully');
+        }
+        else{
+            return redirect(route('admin.users'))->with('status','User Not Found');
+        }
+    }
 }
